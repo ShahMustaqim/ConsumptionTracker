3408e05173703ec15b6496b4aa0498e3c86bc69e
Shah <shah.must.aqims@gmail.com>
2026-04-20 16:43:17 +0100

    Initial Commit

---
+++ ConsumptionTracker/ConsumptionTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsumptionTrackerApp.swift
+//  ConsumptionTracker
+//
+//  Created by Shahril S M B (FCES) on 20/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ConsumptionTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
