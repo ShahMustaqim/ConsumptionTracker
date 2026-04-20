d0ac1581125d7cf943aac033489553b461de1576
Shah <shah.must.aqims@gmail.com>
2026-04-20 18:25:30 +0100

    added input form

---
+++ ConsumptionTracker/AddRecordView.swift
@@ -0,0 +1,18 @@
+//
+//  AddRecordView.swift
+//  ConsumptionTracker
+//
+//  Created by Shahril S M B (FCES) on 20/04/2026.
+//
+
+import SwiftUI
+
+struct AddRecordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddRecordView()
+}
