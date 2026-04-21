0481a313b699de9f068b883b193c05f09975436d
Shah <shah.must.aqims@gmail.com>
2026-04-21 17:20:36 +0100

    added charts and a chartview

---
+++ ConsumptionTracker/ChartView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartView.swift
+//  ConsumptionTracker
+//
+//  Created by Shahril S M B (FCES) on 21/04/2026.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChartView()
+}
