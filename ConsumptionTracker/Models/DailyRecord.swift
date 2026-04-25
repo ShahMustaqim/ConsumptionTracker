24811e36c05de5f4f5f5dc0316848c1be2f7fc93
Shah Mustaqim <147836220+ShahMustaqim@users.noreply.github.com>
2026-04-25 20:34:29 +0100

    placing comments as notes
    Updated comments for clarity and adjusted variable setter visibility to eliminate warnings.

--- ConsumptionTracker/Models/DailyRecord.swift
+++ ConsumptionTracker/Models/DailyRecord.swift
@@ -5,11 +5,11 @@
 //  Created by Shahril S M B (FCES) on 20/04/2026.
 //
 import Foundation
-import SwiftData
+import SwiftData//to use database features
 
 @Model
 class DailyRecord:Identifiable{
-    private(set) var id = UUID()
+    private(set) var id = UUID()//variable setter private to get rid of warning
     var date : Date
     var waterConsumed : Int
     var caloriesConsumed : Int
